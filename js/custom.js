5b82fde647fdb2cb071009f526424f9253ed51e7
Kleistr <lingyu2586@gmail.com>
2022-08-08 00:42:13 +0800

    Site updated: 2022-08-08 00:42:13

--- js/custom.js
+++ js/custom.js
@@ -9,6 +9,46 @@ window.onload = function checkTime() {
 		document.cookie = "night=0;path=/"
 	}
 }
+function getCookie(cname)
+{
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0; i<ca.length; i++) 
+  {
+    var c = ca[i].trim();
+    if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+  }
+  return "";
+}
+window.onload = function changeBackgroundImage() {
+	
+	var nightOrNot = getCookie('night');
+	var bodyBgs = [];
+	if (nightOrNot == 0){
+		bodyBgs[0] = "bg/001.jpg";
+		bodyBgs[1] = "bg/002.jpg";
+		bodyBgs[2] = "bg/003.png";
+		bodyBgs[3] = "bg/004.jpg";
+		bodyBgs[4] = "bg/005.jpg";
+		bodyBgs[5] = "bg/006.jpg";
+		bodyBgs[6] = "bg/002.jpg";
+	}
+	else {
+		bodyBgs[0] = "bg/007.jpg";
+		bodyBgs[1] = "bg/008.jpg";
+		bodyBgs[2] = "https://kleist-blog.oss-cn-beijing.aliyuncs.com/BlogPic/IMG_2854-2020-08-26.PNG";
+		bodyBgs[3] = "bg/009.png";
+		bodyBgs[4] = "bg/010.jpg";
+		bodyBgs[5] = "bg/011.png";
+		bodyBgs[6] = "bg/012.jpg";
+	}
+	
+	var randomBgIndex = Math.round(Math.random() * 6);
+	//输出随机的背景图
+	$("div.bg").css("background","url(" + bodyBgs[randomBgIndex]+")	no-repeat center center");
+	// $("div.bg").css("background-size","cover");
+}
+
 
 // 鼠标滑过网址卡片、输入框触发背景模糊 -----------------------------开始
 // 提示：一定要用querySelector来选择，用其它没效果
